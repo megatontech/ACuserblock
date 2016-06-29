3c32b26e04cab9cbceea18f7089f96eef1fdc406
techme <techme@live.cn>
2016-06-29 14:18:56 +0800

    修改了功能和界面

--- AcfunHelper/js/main.js
+++ AcfunHelper/js/main.js
@@ -16,13 +16,11 @@
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = chrome.extension.getURL('js/detector.js');
-
         var meta = document.createElement('meta');
         meta.name = 'chromesniffer';
         meta.id = 'chromesniffer_meta';
         head.appendChild(meta);
         head.appendChild(script);
-
         meta.addEventListener('ready',
         function() {
             setTimeout(function(){
